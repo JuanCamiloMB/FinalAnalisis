1396905a598094359459f9e03bc2293bde4eaa4b
JuanK <juancamilo.manjarres@gmail.com>
2023-11-23 00:04:10 -0500

    Runge added

--- metodos.py
+++ metodos.py
@@ -67,6 +67,19 @@ def Euler(func,a,b,h,co):
         eu.append(eu[i]+h*f(t[i], eu[i]))
     return eu
 
+def Runge(f,a,b,h,co):
+    n = int((b-a)/h)
+    t = np.linspace(a,b,n+1)
+    rk = [co]
+    for i in range(n):
+        k1=h*f(t[i], rk[i])
+        k2= h*f(t[i]+h/2, rk[i]+1/2*k1)
+        k3= h*f(t[i]+h/2, rk[i]+1/2*k2)
+        k4= h*f(t[i+1], rk[i]+k3)
+        rk.append(rk[i]+1/6*(k1+2*k2+2*k3+k4))
+        print(k1,k2,k3,k4)        
+    return t,rk
+
 ##############################################################################
 
 def NewtonR(f,x0,tol):
